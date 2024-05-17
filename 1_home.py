03e182d155e598e9aa0ec1e6c878afe152305e09
RafaelOpytCorporativo <156432620+RafaelOpytCorporativo@users.noreply.github.com>
2024-05-17 17:38:26 -0300

    Adding save messages func
    Adding a function to save messages

--- 1_home.py
+++ 1_home.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import os
 import openai
+import re
+from unidecode import unidecode
+import pickle
+from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 
-
+MESSAGES_FOLDER=Path(__file__).parent / 'messages'
+MESSAGES_FOLDER.mkdir(exist_ok=True)
 openai.api_key = os.environ['OPENAI_API_KEY']
 def get_model_response(
   messages,
@@ -20,13 +25,47 @@ def get_model_response(
   )
   return response
 
+def convert_name_message(message_name):
+    file_name = unidecode(message_name)
+    file_name = re.sub('\W+', '', file_name).lower()
+    return file_name
+
+def get_chat_name(messages):
+  message_name = ''
+  for message in messages:
+    if message['role'] == 'user':
+      message_name = message['content'][:30]
+      break
+  return message_name
+
+def save_messages(messages):
+  if len(messages) <= 0:
+    return False
+  message_name = get_chat_name(messages)
+  file_name = convert_name_message(message_name)
+  file_to_save = {
+                  'message_name': message_name, 
+                  'file_name': file_name,
+                  'messages': messages
+                }
+  with open(MESSAGES_FOLDER / file_name, 'wb') as f:
+    pickle.dump(file_to_save, f)
+
+def read_messages(messages, key='messages'):
+  if len(messages) <= 0:
+    return []
+  message_name = get_chat_name(messages)
+  file_name = convert_name_message(message_name)
+  with open(MESSAGES_FOLDER / file_name, 'rb') as f:
+    messages = pickle.load(f)
+  return messages[key]
 
 def pagina_principal():
   
   if not 'messages' in st.session_state:
     st.session_state['messages'] = []
   
-  messages = st.session_state['messages']
+  messages = read_messages(st.session_state['messages'])
   
   st.header('GPT Chatbot Training Copy')
   
@@ -58,6 +97,7 @@ def pagina_principal():
     messages.append(new_response)
     
     st.session_state['messages'] = messages
+    save_messages(messages)
 
 
 pagina_principal()
